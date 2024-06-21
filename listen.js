8546decbb10513ac0baeff41531540e43ded5af0
George Grady <george.grady@hotmail.com>
2024-06-21 18:21:29 +0100

    added prepare husky install script

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require("./api/app")
 const {PORT = 9090} = process.env
 
-app.listen(PORT, () => console.log(`Listenign on ${PORT}...`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
